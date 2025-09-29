17cab5d19ce6dec438f4b5b4e9ab71296559025c
hoaithii17 <146211101+hoaithii17@users.noreply.github.com>
2025-09-29 22:56:15 +0700

    Update app.js

--- app.js
+++ app.js
@@ -8,16 +8,19 @@ app.set("views", path.join(__dirname, "views"));
 
 // Public folder
 app.use(express.static(path.join(__dirname, "public")));
+
+// Tắt cache (không bắt buộc, nhưng bạn có thêm)
 app.use((req, res, next) => {
-  res.set('Cache-Control', 'no-store, no-cache, must-revalidate, private');
+  res.set("Cache-Control", "no-store, no-cache, must-revalidate, private");
   next();
 });
+
 // Routes
 const cvRoutes = require("./routes/cvRoutes");
 app.use("/", cvRoutes);
 
-// Start server
-const PORT = 3000;
+// Start server (Render sẽ cấp PORT động)
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
